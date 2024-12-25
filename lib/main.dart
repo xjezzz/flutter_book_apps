6efa249e416ff10e4c84a2ad21e384df7fe125fe
Ернур Мухаметрахимов <e.mukhametrakhimov@corp.cyberone.kz>
2024-12-25 15:45:05 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import "dart:io";
+import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
+// import "appointments/Appointments.dart";
+// import "contacts/Contacts.dart";
+// import "notes/Notes.dart";
+// import "tasks/Tasks.dart";
+import "utils.dart" as utils;
+void main() {
+	 startMeUp() async {
+	 	//  Directory docsDir =
+	 	//  	 await getApplicationDocumentsDirectory();
+	 	//  utils.docsDir = docsDir;
+	 	 runApp(FlutterBook());
+}
+	 startMeUp();
+}
+
+class FlutterBook extends StatelessWidget {
+	 Widget build(BuildContext inContext) {
+ return MaterialApp(
+	 	 	 home : DefaultTabController(
+	 	 	 	 length : 4,
+ child : Scaffold(
+ appBar : AppBar(
+	 	 	 	 	 	 title : Text("FlutterBook"),
+ bottom : TabBar(
+	 	 	 	 	 	 	 tabs : [
+	 	 	 	 	 	 	 	 Tab(icon : Icon(Icons.date_range),
+	 	 	 	 	 	 	 	 	 text : "Appointments"),
+	 	 	 	 	 	 	 	 Tab(icon : Icon(Icons.contacts),
+	 	 	 	 	 	 	 	 	 text : "Contacts"),
+	 	 	 	 	 	 	 	 Tab(icon : Icon(Icons.note),
+	 	 	 	 	 	 	 	 	 text : "Notes"),
+	 	 	 	 	 	 	 	 Tab(icon : Icon(Icons.assignment_turned_in),
+	 	 	 	 	 	 	 	 	 text : "Tasks")
+							]
+						)
+	 	 	 	 	 ),
+	 	 	 	 	 body : TabBarView(
+	 	 	 	 	 	 children : [
+	 	 	 	 	 	 	//  Appointments(), Contacts(), Notes(), Tasks()
+						]
+					)
+				)
+			)
+	 	 );
+}
+}
